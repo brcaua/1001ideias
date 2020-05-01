5375e4cddb078b8fc30d9ab6159ba851ea7284fd
Breno Cauã <progbrcaua@gmail.com>
2020-05-01 18:58:45 -0300

    🚀 RELEASE: v2.0

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 //Função de pop-up para o botão primário
 function onOff() {
-  document
-    .querySelector("#modal")
-    .classList
-    .toggle("hide")
-  document
-    .querySelector("body")
-    .classList
-    .toggle("hideScroll")
-  document
-    .querySelector("#modal")
-    .classList
-    .toggle("addScroll")
+  document.querySelector("#modal").classList.toggle("hide");
+  document.querySelector("body").classList.toggle("hideScroll");
+  document.querySelector("#modal").classList.toggle("addScroll");
 }
